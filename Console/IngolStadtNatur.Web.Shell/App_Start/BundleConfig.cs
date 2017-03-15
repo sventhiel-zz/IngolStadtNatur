235848909f0824f028d34b4141a9277532f89cab
Sven Thiel <sven.thiel@uni-jena.de>
2017-03-15 15:37:01 +0100

    update packages

--- Console/IngolStadtNatur.Web.Shell/App_Start/BundleConfig.cs
+++ Console/IngolStadtNatur.Web.Shell/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace IngolStadtNatur.Web.Shell
 {
@@ -9,23 +8,44 @@ namespace IngolStadtNatur.Web.Shell
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                "~/Scripts/jquery.validate.*",
+                "~/Scripts/mvcfoolproof.unobtrusive.min.js"));
 
             // Verwenden Sie die Entwicklungsversion von Modernizr zum Entwickeln und Erweitern Ihrer Kenntnisse. Wenn Sie dann
             // für die Produktion bereit sind, verwenden Sie das Buildtool unter "http://modernizr.com", um nur die benötigten Tests auszuwählen.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                "~/Scripts/bootstrap.min.js",
+                "~/Scripts/bootstrap-datepicker.min.js",
+                "~/Scripts/locales/bootstrap-datepicker.de.min.js",
+                "~/Scripts/lightslider.min.js",
+                "~/Scripts/bootstrap3-typeahead.min.js",
+                "~/Scripts/respond.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/leaflet").Include(
+                "~/Scripts/leaflet.js",
+                "~/Scripts/leaflet-manager.js",
+                "~/Scripts/leaflet.geojsoncss.min.js",
+                "~/Scripts/habitats.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/moment").Include(
+                "~/Scripts/moment.min.js"));
+
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                "~/Content/bootstrap.min.css",
+                "~/Content/lightslider.min.css",
+                "~/Content/bootstrap-datepicker.min.css",
+                "~/Content/leaflet.css",
+                "~/Content/font-awesome.min.css",
+                "~/Content/Site.css"));
+
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
