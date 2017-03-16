5a553836c5c7bdb86c81cfc2476463515bcccf26
sventhiel <sven.thiel@uni-jena.de>
2017-03-16 10:16:40 +0100

    update user store

---
+++ Components/IngolStadtNatur.Entities.NH/Authentication/Login.cs
@@ -0,0 +1,11 @@
+﻿using IngolStadtNatur.Entities.NH.Common;
+
+namespace IngolStadtNatur.Entities.NH.Authentication
+{
+    public class Login : BaseEntity
+    {
+        public virtual string LoginProvider { get; set; }
+        public virtual string ProviderKey { get; set; }
+        public virtual User User { get; set; }
+    }
+}
