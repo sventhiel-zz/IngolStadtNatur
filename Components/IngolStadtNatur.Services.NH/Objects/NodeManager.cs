313a2c9ad59e9a9de46427548f3dcec5620106bf
sventhiel <sven.thiel@uni-jena.de>
2017-03-17 15:03:27 +0100

    update managers

--- Components/IngolStadtNatur.Services.NH/Objects/NodeManager.cs
+++ Components/IngolStadtNatur.Services.NH/Objects/NodeManager.cs
@@ -7,14 +7,19 @@ namespace IngolStadtNatur.Services.NH.Objects
 {
     public class NodeManager : INodeManager
     {
+        public Repository<Category> CategoryRepository { get; set; }
         public Repository<Node> NodeRepository { get; set; }
+        public Repository<Species> SpeciesRepository { get; set; }
 
         public NodeManager()
         {
             NodeRepository = new Repository<Node>();
+            SpeciesRepository = new Repository<Species>();
         }
 
+        public IQueryable<Category> Categories => CategoryRepository.Query();
         public IQueryable<Node> Nodes => NodeRepository.Query();
+        public IQueryable<Species> Species => SpeciesRepository.Query();
 
         public void Create(Node entity)
         {
@@ -24,6 +29,26 @@ namespace IngolStadtNatur.Services.NH.Objects
         public void Delete(Node entity)
         {
             NodeRepository.Remove(entity);
+        }
+
+        public Category GetCategory(long id)
+        {
+            return CategoryRepository.Get(id);
+        }
+
+        public Node GetNode(long id)
+        {
+            return NodeRepository.Get(id);
+        }
+
+        public Category GetRoot()
+        {
+            return Categories.FirstOrDefault(m => m.Parent == null);
+        }
+
+        public Species GetSpecies(long id)
+        {
+            return SpeciesRepository.Get(id);
         }
 
         public void Update(Node entity)
--- Console/IngolStadtNatur.Web.Shell/Controllers/ObservationController.cs
+++ Console/IngolStadtNatur.Web.Shell/Controllers/ObservationController.cs
@@ -1,17 +1,162 @@
-﻿using System;
+﻿using IngolStadtNatur.Entities.NH.Objects;
+using IngolStadtNatur.Services.NH.Objects;
+using IngolStadtNatur.Services.NH.Observations;
+using IngolStadtNatur.Web.Shell.Models;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace IngolStadtNatur.Web.Shell.Controllers
 {
     public class ObservationController : Controller
     {
-        // GET: Observation
         public ActionResult Index()
         {
+            var nodeManager = new NodeManager();
+            return View("CategoryGroupList", CategoryListGroupModel.Convert(nodeManager.GetRoot()));
+        }
+
+        [ChildActionOnly]
+        public ActionResult GetCategoryChild(long id)
+        {
+            var nodeManager = new NodeManager();
+            var node = nodeManager.GetNode(id);
+
+            if (node is Category)
+            {
+                return PartialView("_CategoryGroupListItem", CategoryListGroupItemModel.Convert((Category)node));
+            }
+            else
+            {
+                return PartialView("_SpeciesGroupListItem", SpeciesListGroupItemModel.Convert((Species)node));
+            }
+        }
+
+        public ActionResult Category(long id)
+        {
+            NodeManager nodeManager = new NodeManager();
+            return View("CategoryGroupList", CategoryListGroupModel.Convert(nodeManager.GetCategory(id)));
+        }
+
+
+
+        public ActionResult Thanks()
+        {
             return View();
+        }
+
+        public ActionResult CreateCategoryObservation(long id)
+        {
+            NodeManager nodeManager = new NodeManager();
+            return View("CreateCategoryObservation", CreateCategoryObservationModel.Convert(nodeManager.GetCategory(id)));
+        }
+
+        [HttpPost]
+        public ActionResult CreateCategoryObservation(CreateCategoryObservationModel model)
+        {
+            NodeManager nodeManager = new NodeManager();
+
+            if (ModelState.IsValid)
+            {
+                ObservationManager observationManager = new ObservationManager();
+                var observation = observationManager.CreateCategoryObservation(nodeManager.GetCategoryById(model.Category.Id), model.Comment, model.Coordinates, Origin.Category, model.Species);
+
+                if (model.Shot != null)
+                {
+                    ShotManager shotManager = new ShotManager(MvcApplication.SessionFactory);
+                    var fileName = observation.Id + "_Shot" + Path.GetExtension(model.Shot.FileName);
+                    var path = Path.Combine(ConfigurationManager.AppSettings["Workspace"], fileName);
+
+                    model.Shot.SaveAs(path);
+
+                    shotManager.CreateShot(observation.Id, fileName, path);
+                }
+
+                return RedirectToAction("Thanks", "Observation");
+            }
+
+            model.Category = CategoryModel.Convert(nodeManager.GetCategoryById(model.Category.Id));
+            return View("CreateCategoryObservation", model);
+        }
+
+        [NHibernatePersistence]
+        public ActionResult CreateQuickObservation()
+        {
+            return View("CreateQuickObservation", new CreateQuickObservationModel());
+        }
+
+        [HttpPost]
+        [NHibernatePersistence]
+        public ActionResult CreateQuickObservation(CreateQuickObservationModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                NodeManager nodeManager = new NodeManager(MvcApplication.SessionFactory);
+                ObservationManager observationManager = new ObservationManager(MvcApplication.SessionFactory);
+                var observation = observationManager.CreateCategoryObservation(nodeManager.GetRoot(), model.Comment, model.Coordinates, Origin.Quick, model.Species);
+
+                if (model.Shot != null)
+                {
+                    ShotManager shotManager = new ShotManager(MvcApplication.SessionFactory);
+                    var fileName = observation.Id + "_Shot" + Path.GetExtension(model.Shot.FileName);
+                    var path = Path.Combine(ConfigurationManager.AppSettings["Workspace"], fileName);
+
+                    model.Shot.SaveAs(path);
+
+                    shotManager.CreateShot(observation.Id, fileName, path);
+                }
+
+                return RedirectToAction("Thanks", "Observation");
+            }
+
+            return View("CreateQuickObservation", model);
+        }
+
+        [NHibernatePersistence]
+        public ActionResult CreateSpeciesObservation(long id)
+        {
+            NodeManager nodeManager = new NodeManager(MvcApplication.SessionFactory);
+            return View("CreateSpeciesObservation", CreateSpeciesObservationModel.Convert(nodeManager.GetSpeciesById(id)));
+        }
+
+        [HttpPost]
+        [NHibernatePersistence]
+        public ActionResult CreateSpeciesObservation(CreateSpeciesObservationModel model)
+        {
+            NodeManager nodeManager = new NodeManager(MvcApplication.SessionFactory);
+
+            if (ModelState.IsValid)
+            {
+                ObservationManager observationManager = new ObservationManager(MvcApplication.SessionFactory);
+                var observation = observationManager.CreateSpeciesObservation(nodeManager.GetSpeciesById(model.Species.Id), model.Comment, model.Coordinates, Origin.Species);
+
+                if (model.Shot != null)
+                {
+                    ShotManager shotManager = new ShotManager(MvcApplication.SessionFactory);
+                    var fileName = observation.Id + "_Shot" + Path.GetExtension(model.Shot.FileName);
+                    var path = Path.Combine(ConfigurationManager.AppSettings["Workspace"], fileName);
+
+                    model.Shot.SaveAs(path);
+
+                    shotManager.CreateShot(observation.Id, fileName, path);
+                }
+
+                return RedirectToAction("Thanks", "Observation");
+            }
+
+            model.Species = SpeciesModel.Convert(nodeManager.GetSpeciesById(model.Species.Id));
+            return View("CreateSpeciesObservation", model);
+        }
+
+        public JsonResult GetSpeciesNames(string query)
+        {
+            NodeManager nodeManager = new NodeManager(MvcApplication.SessionFactory);
+
+            List<string> commonNames = nodeManager.GetSpecies().Select(m => m.CommonName).Where(m => m.ToLower().Contains(query.ToLower())).ToList();
+            List<string> scientificNames = nodeManager.GetSpecies().Select(m => m.ScientificName).Where(m => m.ToLower().Contains(query.ToLower())).ToList();
+            return this.Json(commonNames.Union(scientificNames), JsonRequestBehavior.AllowGet);
         }
     }
 }
