1afbff8ea0d793cee1e64266ff9e944fc69f24b5
sventhiel <sven.thiel@uni-jena.de>
2017-03-21 14:47:37 +0100

    add login mapping

--- Components/IngolStadtNatur.Entities.NH/Authentication/Login.cs
+++ Components/IngolStadtNatur.Entities.NH/Authentication/Login.cs
@@ -1,4 +1,5 @@
-﻿using IngolStadtNatur.Entities.NH.Common;
+﻿using FluentNHibernate.Mapping;
+using IngolStadtNatur.Entities.NH.Common;
 
 namespace IngolStadtNatur.Entities.NH.Authentication
 {
@@ -7,5 +8,22 @@ namespace IngolStadtNatur.Entities.NH.Authentication
         public virtual string LoginProvider { get; set; }
         public virtual string ProviderKey { get; set; }
         public virtual User User { get; set; }
+    }
+
+    public class LoginMap : ClassMap<Login>
+    {
+        public LoginMap()
+        {
+            Table("Logins");
+
+            Id(m => m.Id);
+            Version(m => m.Version);
+
+            Map(m => m.LoginProvider);
+            Map(m => m.ProviderKey);
+            References(m => m.User)
+                .Column("UserRef")
+                .Cascade.All();
+        }
     }
 }
