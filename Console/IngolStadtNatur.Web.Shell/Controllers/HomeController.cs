97128af2c1124ed1dae60146a374154cff0f70fe
sventhiel <sven.thiel@uni-jena.de>
2017-03-29 10:56:41 +0200

    fix impressum link

--- Console/IngolStadtNatur.Web.Shell/Controllers/HomeController.cs
+++ Console/IngolStadtNatur.Web.Shell/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace IngolStadtNatur.Web.Shell.Controllers
 {
@@ -13,17 +9,8 @@ namespace IngolStadtNatur.Web.Shell.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Impressum()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
