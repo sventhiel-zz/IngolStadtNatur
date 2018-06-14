dc882d7da15f480d2fbefa9047382ad0ade0422e
Sven Thiel <sven.thiel@uni-jena.de>
2018-06-14 15:37:46 +0200

    #9 finalized backend of first implementation of public/private gallery

--- Console/IngolStadtNatur.Web.Shell/Controllers/GalleryController.cs
+++ Console/IngolStadtNatur.Web.Shell/Controllers/GalleryController.cs
@@ -1,6 +1,5 @@
 ﻿using IngolStadtNatur.Services.NH.Media;
 using IngolStadtNatur.Web.Shell.Models;
-using System;
 using System.Linq;
 using System.Linq.Dynamic;
 using System.Web.Mvc;
@@ -9,12 +8,6 @@ namespace IngolStadtNatur.Web.Shell.Controllers
 {
     public class GalleryController : Controller
     {
-        // GET: Gallery
-        public ActionResult Index()
-        {
-            return View();
-        }
-
         [Authorize]
         public ActionResult Private()
         {
@@ -31,11 +24,6 @@ namespace IngolStadtNatur.Web.Shell.Controllers
             var shots = shotManager.Shots.Where(s => s.IsPublic);
 
             return View(shots.AsEnumerable().Select(GalleryItemModel.Convert).ToList());
-        }
-
-        public ActionResult Select_Shots()
-        {
-            throw new NotImplementedException();
         }
     }
 }
--- Console/IngolStadtNatur.Web.Shell/Models/GalleryModels.cs
+++ Console/IngolStadtNatur.Web.Shell/Models/GalleryModels.cs
@@ -1,17 +1,16 @@
 ﻿using IngolStadtNatur.Entities.NH.Media;
-using System.Configuration;
 
 namespace IngolStadtNatur.Web.Shell.Models
 {
     public class GalleryItemModel
     {
-        public string Path { get; set; }
+        public string Name { get; set; }
 
         public static GalleryItemModel Convert(Shot shot)
         {
             return new GalleryItemModel()
             {
-                Path = System.IO.Path.Combine(ConfigurationManager.AppSettings["Shots"], shot.Name)
+                Name = shot.Name
             };
         }
     }
